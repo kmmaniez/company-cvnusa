d93ab4e5e9d9a4aac406227a3879a242386c2f49
kmmaniez <dzlabsid@gmail.com>
2023-08-03 18:51:36 +0700

    public about

--- resources/views/public/about.blade.php
+++ resources/views/public/about.blade.php
@@ -1,7 +1,6 @@
-@extends('layouts.master')
+@extends('layouts.public.master')
 
 @section('content')
-    
     <div id="banner-area" class="banner-area" style="background-image:url({{ url('assets/images/banner/banner1.jpg') }})">
         <div class="banner-text">
             <div class="container">
@@ -26,119 +25,53 @@
     <section id="main-container" class="main-container">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6">
-                    <h3 class="column-title">Who We Are</h3>
+                <div class="col-lg-12 text-center">
+                    <h3>Who We Are</h3>
                     <p>when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a
                         horrible vermin.</p>
-                    <blockquote>
-                        <p>Semporibus autem quibusdam et aut officiis debitis aut rerum est aut optio cumque nihil
-                            necessitatibus autemn ec tincidunt nunc posuere ut</p>
-                    </blockquote>
                     <p>He lay on his armour-like back, and if he lifted. ultrices ultrices sapien, nec tincidunt
                         nunc posuere ut. Lorem ipsum dolor sit amet, consectetur adipiscing elit. If you are going
                         to use a passage of Lorem Ipsum, you need to be sure there isn’t anything embarrassing.</p>
-
-                </div><!-- Col end -->
-
-                <div class="col-lg-6 mt-5 mt-lg-0">
-
-                    <div id="page-slider" class="page-slider small-bg">
-
-                        <div class="item" style="background-image:url({{ url('assets/images/slider-pages/slide-page1.jpg') }})">
-                            <div class="container">
-                                <div class="box-slider-content">
-                                    <div class="box-slider-text">
-                                        <h2 class="box-slide-title">Leadership</h2>
-                                    </div>
-                                </div>
+                </div>
+                <div class="col-lg-12 mt-5 text-justify">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <h3>Our Vision</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus accusamus adipisci
+                                sapiente earum, voluptate qui consequuntur quae voluptates cupiditate molestias delectus
+                                harum magni velit nulla itaque ab ullam dolore? Suscipit ipsa impedit illo tempore cum
+                                provident ducimus fugit at laboriosam?</p>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum aliquam nihil, aliquid
+                                asperiores cupiditate doloribus? Eum iure nemo ab quos!</p>
+                        </div>
+                        <div class="col-lg-6">
+                            <h3>Our Mission</h3>
+                            <div class="content">
+                                <ul>
+                                    <li style="font-size: 1rem;">Lorem ipsum dolor sit amet.</li>
+                                    <li style="font-size: 1rem;">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                        Delectus officiis ad eligendi, deserunt quos omnis.</li>
+                                    <li style="font-size: 1rem">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                        Recusandae, quaerat.</li>
+                                    <li style="font-size: 1rem">Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+                                    <li style="font-size: 1rem">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                        Nesciunt.</li>
+                                </ul>
                             </div>
-                        </div><!-- Item 1 end -->
+                        </div>
+                    </div>
+                </div>
 
-                        <div class="item" style="background-image:url({{ url('assets/images/slider-pages/slide-page2.jpg') }})">
-                            <div class="container">
-                                <div class="box-slider-content">
-                                    <div class="box-slider-text">
-                                        <h2 class="box-slide-title">Relationships</h2>
-                                    </div>
-                                </div>
-                            </div>
-                        </div><!-- Item 1 end -->
-
-                        <div class="item" style="background-image:url({{ url('assets/images/slider-pages/slide-page3.jpg') }})">
-                            <div class="container">
-                                <div class="box-slider-content">
-                                    <div class="box-slider-text">
-                                        <h2 class="box-slide-title">Performance</h2>
-                                    </div>
-                                </div>
-                            </div>
-                        </div><!-- Item 1 end -->
-                    </div><!-- Page slider end-->
-
-
-                </div><!-- Col end -->
             </div><!-- Content row end -->
 
         </div><!-- Container end -->
     </section><!-- Main container end -->
 
-
-    <section id="facts" class="facts-area dark-bg">
-        <div class="container">
-            <div class="facts-wrapper">
-                <div class="row">
-                    <div class="col-md-3 col-sm-6 ts-facts">
-                        <div class="ts-facts-img">
-                            <img loading="lazy" src="{{ url('assets/images/icon-image/fact1.png') }}" alt="facts-img">
-                        </div>
-                        <div class="ts-facts-content">
-                            <h2 class="ts-facts-num"><span class="counterUp" data-count="1789">0</span></h2>
-                            <h3 class="ts-facts-title">Total Projects</h3>
-                        </div>
-                    </div><!-- Col end -->
-
-                    <div class="col-md-3 col-sm-6 ts-facts mt-5 mt-sm-0">
-                        <div class="ts-facts-img">
-                            <img loading="lazy" src="{{ url('assets/images/icon-image/fact2.png') }}" alt="facts-img">
-                        </div>
-                        <div class="ts-facts-content">
-                            <h2 class="ts-facts-num"><span class="counterUp" data-count="647">0</span></h2>
-                            <h3 class="ts-facts-title">Staff Members</h3>
-                        </div>
-                    </div><!-- Col end -->
-
-                    <div class="col-md-3 col-sm-6 ts-facts mt-5 mt-md-0">
-                        <div class="ts-facts-img">
-                            <img loading="lazy" src="{{ url('assets/images/icon-image/fact3.png') }}" alt="facts-img">
-                        </div>
-                        <div class="ts-facts-content">
-                            <h2 class="ts-facts-num"><span class="counterUp" data-count="4000">0</span></h2>
-                            <h3 class="ts-facts-title">Hours of Work</h3>
-                        </div>
-                    </div><!-- Col end -->
-
-                    <div class="col-md-3 col-sm-6 ts-facts mt-5 mt-md-0">
-                        <div class="ts-facts-img">
-                            <img loading="lazy" src="{{ url('assets/images/icon-image/fact4.png') }}" alt="facts-img">
-                        </div>
-                        <div class="ts-facts-content">
-                            <h2 class="ts-facts-num"><span class="counterUp" data-count="44">0</span></h2>
-                            <h3 class="ts-facts-title">Countries Experience</h3>
-                        </div>
-                    </div><!-- Col end -->
-
-                </div> <!-- Facts end -->
-            </div>
-            <!--/ Content row end -->
-        </div>
-        <!--/ Container end -->
-    </section><!-- Facts end -->
-
     <section id="ts-team" class="ts-team">
         <div class="container">
             <div class="row text-center">
                 <div class="col-lg-12">
-                    <h2 class="section-title">Quality Service</h2>
+                    <h2 class="section-title">Team Service</h2>
                     <h3 class="section-sub-title">Professional Team</h3>
                 </div>
             </div>
@@ -258,7 +191,7 @@
                             </div>
                             <!--/ Team wrapper end -->
                         </div><!-- Team 5 end -->
-                        
+
                     </div><!-- Team slide end -->
                 </div>
             </div>
@@ -268,6 +201,3 @@
     </section>
     <!--/ Team end -->
 @endsection
-
-
-    
