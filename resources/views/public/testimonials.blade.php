dc22038f893fcb645b037018317329e4102ea337
kmmaniez <dzlabsid@gmail.com>
2023-08-03 18:51:57 +0700

    public testimonial

---
+++ resources/views/public/testimonials.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.public.master')
+
+@section('content')
+    <div id="banner-area" class="banner-area" style="background-image:url({{ asset('assets/images/banner/banner1.jpg') }})">
+        <div class="banner-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="banner-heading">
+                            <h1 class="banner-title">Clients</h1>
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb justify-content-center">
+                                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                    <li class="breadcrumb-item"><a href="#">company</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Clients</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div><!-- Col end -->
+                </div><!-- Row end -->
+            </div><!-- Container end -->
+        </div><!-- Banner text end -->
+    </div><!-- Banner area end -->
+
+    <x-public.testimonial></x-public.testimonial>
+@endsection
