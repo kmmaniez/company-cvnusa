c704b7cc9beaf3fe6b4ae0d9c918e56da03c3190
kmmaniez <dzlabsid@gmail.com>
2023-08-05 15:56:15 +0700

    components for public carousel

--- resources/views/components/public/carousel.blade.php
+++ resources/views/components/public/carousel.blade.php
@@ -8,8 +8,8 @@
                         <h3 class="slide-sub-title" data-animation-in="slideInRight">Construction Industry
                         </h3>
                         <p data-animation-in="slideInLeft" data-duration-in="1.2">
-                            <a href="services.html" class="slider btn btn-primary">Our Services</a>
-                            <a href="contact.html" class="slider btn btn-primary border">Contact Now</a>
+                            <a href="{{ route('public.services') }}" class="slider btn btn-primary">Our Services</a>
+                            <a href="{{ route('public.contact') }}" class="slider btn btn-primary border">Contact Now</a>
                         </p>
                     </div>
                 </div>
@@ -26,7 +26,7 @@
                         <h3 class="slide-title" data-animation-in="fadeIn">When Service Matters</h3>
                         <h3 class="slide-sub-title" data-animation-in="slideInLeft">Your Choice is Simple</h3>
                         <p data-animation-in="slideInRight">
-                            <a href="services.html" class="slider btn btn-primary border">Our Services</a>
+                            <a href="{{ route('public.services') }}" class="slider btn btn-primary border">Our Services</a>
                         </p>
                     </div>
                 </div>
@@ -44,9 +44,7 @@
                         <p class="slider-description lead" data-animation-in="slideInRight">We will deal with
                             your failure that determines how you achieve success.</p>
                         <div data-animation-in="slideInLeft">
-                            <a href="contact.html" class="slider btn btn-primary"
-                                aria-label="contact-with-us">Get Free Quote</a>
-                            <a href="about.html" class="slider btn btn-primary border"
+                            <a href="{{ route('public.about') }}" class="slider btn btn-primary border"
                                 aria-label="learn-more-about-us">Learn more</a>
                         </div>
                     </div>
