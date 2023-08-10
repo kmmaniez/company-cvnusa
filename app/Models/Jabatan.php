fbbb89c4034d378ab7bd3f8e90e8e6124777f65c
kmmaniez <dzlabsid@gmail.com>
2023-08-10 09:45:36 +0700

    update model jabatan & remove unused models

--- app/Models/Jabatan.php
+++ app/Models/Jabatan.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Jabatan extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+
+    // public function anggota()
+    // {
+    //     return $this->belongsToMany(Anggota::class);
+    // }
 }
