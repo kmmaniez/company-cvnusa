85dbb90aaca74844dd6906d7a8488cc15bf89a06
kmmaniez <dzlabsid@gmail.com>
2023-08-10 09:14:56 +0700

    migration projects

---
+++ database/migrations/2023_07_29_053359_create_projects_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_project');
+            $table->string('keterangan_project');
+            $table->date('start_project')->nullable();
+            $table->date('finish_project');
+            $table->text('gambar_project');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
