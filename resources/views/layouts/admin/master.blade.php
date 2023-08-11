1177f5cbe6e29ebde0946e3684b4a098a7d3f359
kmmaniez <dzlabsid@gmail.com>
2023-08-11 23:07:42 +0700

    replace sweetalert package from package to download file

--- resources/views/layouts/admin/master.blade.php
+++ resources/views/layouts/admin/master.blade.php
@@ -103,6 +103,9 @@
         </div>
     </div>
 
+    <!-- SWEETALERT -->
+    {{-- @include('sweetalert::alert') --}}
+
     <!-- Bootstrap core JavaScript-->
     <script src="{{ url('sb-admin') }}/vendor/jquery/jquery.min.js"></script>
     <script src="{{ url('sb-admin') }}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -113,13 +116,13 @@
     <!-- Custom scripts for all pages-->
     <script src="{{ url('sb-admin') }}/js/sb-admin-2.min.js"></script>
 
-    <!-- Page level plugins -->
+    <!-- Vendor scripts -->
+    <script src="{{ asset('assets/sweetalert/dist/sweetalert2.all.min.js') }}"></script>
     {{-- <script src="{{ url('sb-admin') }}/vendor/chart.js/Chart.min.js"></script> --}}
 
     <!-- Page level custom scripts -->
     {{-- <script src="{{ url('sb-admin') }}/js/demo/chart-area-demo.js"></script>
     <script src="{{ url('sb-admin') }}/js/demo/chart-pie-demo.js"></script> --}}
-
     @stack('scripts')
 </body>
 {{-- @vite('resources/js/app.js') --}}
