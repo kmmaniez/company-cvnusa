c3f48a8be23c87702483e5efeaf355965156afaa
kmmaniez <dzlabsid@gmail.com>
2023-08-11 23:16:34 +0700

    add group controller for blog

---
+++ app/Http/Controllers/Admin/Blog/KategoriBlogController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KategoriBlog;
+use Illuminate\Http\Request;
+
+class KategoriBlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(KategoriBlog $kategoriBlog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(KategoriBlog $kategoriBlog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, KategoriBlog $kategoriBlog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(KategoriBlog $kategoriBlog)
+    {
+        //
+    }
+}
