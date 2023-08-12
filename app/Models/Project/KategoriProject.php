9ff59b04eb579203294dfb70f7c40681f450427b
kmmaniez <dzlabsid@gmail.com>
2023-08-12 11:51:16 +0700

    add model group of project

---
+++ app/Models/Project/KategoriProject.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Project;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KategoriProject extends Model
+{
+    use HasFactory;
+    protected $guarded = ['id'];
+}
