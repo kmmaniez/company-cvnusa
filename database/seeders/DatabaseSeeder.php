a72a1fa2e991c27d542a2c23b57d2eabf4d9877f
kmmaniez <dzlabsid@gmail.com>
2023-08-12 09:18:35 +0700

    update db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             ServiceSeeder::class,
             TeamSeeder::class,
             CarouselSeeder::class,
+            BlogSeeder::class,
         ]);
     }
 }
