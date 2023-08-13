a9dbbd47faca412de37d422cc0d7ec112e25c34c
kmmaniez <dzlabsid@gmail.com>
2023-08-13 21:12:07 +0700

    update controller public

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -2,38 +2,80 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Team\Anggota;
+use App\Models\Carousel;
+use App\Models\Clients;
+use App\Models\Kategori;
+use App\Models\Price;
+use App\Models\Project\Project;
+use App\Models\Service;
+use App\Models\WebsiteSetting;
 use Illuminate\Http\Request;
 
 class PublicController extends Controller
 {
+
+    public function __construct()
+    {
+        
+    }
+
     public function index()
     {
-        return view('public.index');
+        return view('public.index',[
+            'client' => Clients::all(),
+            'services' => Service::all('title','description'),
+            'carousels' => Carousel::all('slide_title','slide_subtitle','description','image'),
+            'totalproject' => Project::all()->count(),
+            'totalstaff' => Anggota::all()->count(),
+            'totalclient' => Clients::all()->count(),
+        ]);
     }
     
     public function about()
     {
-        return view('public.about');
+        return view('public.about',[
+            'data' => WebsiteSetting::all(),
+            'teams' => Anggota::all()
+        ]);
     }
 
     public function pricing()
     {
-        return view('public.pricing');
+        return view('public.pricing',[
+            'title' => 'Harga',
+            'prices' => Price::all()
+        ]);
     }
 
     public function services()
     {
-        return view('public.services');
+        return view('public.services',[
+            'services' => Service::all()
+        ]);
     }
 
-    public function testimonial()
+    public function testimonials()
     {
-        return view('public.testimonial');
+        return view('public.testimonials',[
+            'data' => Clients::all()
+        ]);
     }
 
     public function contact()
     {
         return view('public.contact');
+    }
+
+    public function projects(Request $request)
+    {
+        if ($request->has('lists')) {
+            // echo 'projectss';
+        }
+        return view('public.projects',[
+            // 'kategoris' => Kategor::all(),
+            'req' => $request
+        ]);
     }
 
     public function project_details()
