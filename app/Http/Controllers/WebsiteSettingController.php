b567181b9b541f53fa1425fb5ee0b8cb940ec8e0
kmmaniez <dzlabsid@gmail.com>
2023-08-13 21:18:21 +0700

    update websitesetting controller & change a few methods

--- app/Http/Controllers/WebsiteSettingController.php
+++ app/Http/Controllers/WebsiteSettingController.php
@@ -2,17 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Website\UpdateWallpaperRequest;
+use App\Models\Wallpaper;
 use App\Models\WebsiteSetting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class WebsiteSettingController extends Controller
 {
+    private $_website, $_wallpaper;
+
+    public function __construct(WebsiteSetting $websiteSetting, Wallpaper $wallpaper)
+    {
+        $this->_website = $websiteSetting;
+        $this->_wallpaper = $wallpaper;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        return view('admin.web.about', [
+            'title'     => 'About Us Settings',
+            'jmlhdata'  => $this->_website->count(),
+            'data'      => $this->_website->all(),
+        ]);
+    }
+
+    public function indexCarousel()
+    {
+        return view('admin.web.carousel', [
+            'title' => 'Carousel Image'
+        ]);
+    }
+
+    public function indexWallpaper()
+    {
+        return view('admin.web.wallpaper', [
+            'title' => 'Wallpaper Image',
+            'data' => $this->_wallpaper->all()
+        ]);
     }
 
     /**
@@ -28,7 +57,8 @@ class WebsiteSettingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->_website->create($request->all());
+        return redirect()->back();
     }
 
     /**
@@ -37,6 +67,14 @@ class WebsiteSettingController extends Controller
     public function show(WebsiteSetting $websiteSetting)
     {
         //
+    }
+
+    public function showWallpaper($wallpaper)
+    {
+        $web = $this->_wallpaper->find($wallpaper);
+        return response()->json([
+            'data' => $web
+        ]);
     }
 
     /**
@@ -50,9 +88,39 @@ class WebsiteSettingController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, WebsiteSetting $websiteSetting)
+    public function update(Request $request, $id)
     {
-        //
+        $data = $this->_website->firstWhere('id',$id);
+        $data->update($request->except('aboutid'));
+        return redirect()->back();
+    }
+
+    public function updateWallpaper(Request $request, Wallpaper $wallpaper)
+    {
+        
+        if ($request->ajax()) {
+            // $data = $this->_wallpaper->firstWhere('id', $id);
+
+            // if ($request->has('wallpaper_image')) {
+                return response()->json([
+                    'data' => $wallpaper,
+                    'req' => $request->all()
+                ]);
+                // $imgName = strtolower($wallpaper->section_name).'.'.$request->wallpaper_image->extension();
+                // $pathName = Storage::putFileAs('public/wallpaper', $request->file('wallpaper_image'), $imgName);
+                
+                // $data = $this->_wallpaper->update([
+                //     'wallpaper_image' => $pathName
+                // ]);
+
+                // if ($data) {
+                //     return response()->json([
+                //         'data' => $data,
+                //         'req' => $request
+                //     ]);
+                // }
+            // }
+        }
     }
 
     /**
