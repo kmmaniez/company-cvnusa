c08982c4ba82c987402715cba397085275e3f8ef
kmmaniez <dzlabsid@gmail.com>
2023-08-16 16:53:59 +0700

    add views to date created or updated

---
+++ resources/views/admin/blog/datedt.blade.php
@@ -0,0 +1,5 @@
+@props(['created','updated'])
+
+<span class="badge badge-primary p-2">Dibuat {{ $created }}</span>
+<br>
+<span class="badge badge-secondary p-2 mt-1">Diupdate {{ $updated }}</span>
