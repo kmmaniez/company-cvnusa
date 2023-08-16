391818a82cf4aa02e7f4adbab5be6ddb226e9c55
kmmaniez <dzlabsid@gmail.com>
2023-08-16 16:56:18 +0700

    final admin price

--- resources/views/admin/price/index.blade.php
+++ resources/views/admin/price/index.blade.php
@@ -2,11 +2,12 @@
 @push('assets')
     <link href="{{ url('sb-admin') }}/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
     <style>
-        input.form-control.is-invalid:focus{
+        input.form-control.is-invalid:focus {
             /* background-color: red; */
             box-shadow: none;
             border: 1px solid #ff887d;
         }
+
         trix-editor {
             min-height: 100px;
         }
@@ -23,7 +24,7 @@
 
     <div class="row">
 
-        <!-- Area Chart -->
+
         <div class="col-xl-12 col-lg-12">
 
             <div class="card shadow mb-4">
@@ -31,10 +32,9 @@
                     <h6 class="m-0 font-weight-bold text-primary">Tabel Data {{ $title }}</h6>
                 </div>
                 <div class="card-body">
-                    <a href="" class="btn btn-md btn-primary mb-3" id="btnTambahHarga" data-toggle="modal" data-target="#modal-create"><i
-                            class="fas fa-fw fa-plus"></i> Tambah Data</a>
+                    <a href="" class="btn btn-md btn-primary mb-3" id="btnTambahHarga" data-toggle="modal"
+                        data-target="#modal-create"><i class="fas fa-fw fa-plus"></i> Tambah Data</a>
                     <div class="table-responsive">
-                        {{-- @dump($users) --}}
                         <table class="table table-bordered" id="DTPrice" width="100%" cellspacing="0">
                             <thead>
                                 <tr>
@@ -49,12 +49,14 @@
                                 @forelse ($prices as $price)
                                     <tr>
                                         <td>{{ $price->judul }}</td>
-                                        <td>Rp. {{ number_format($price->harga,0,2,',') }}</td>
+                                        <td>Rp. {{ number_format($price->harga, 0, 2, ',') }}</td>
                                         <td>{!! $price->keterangan !!}</td>
                                         <td>{{ $price->custom_text_button }}</td>
                                         <td>
-                                            <a href="#" data-id="{{ $price->id }}" id="btnEditModal" class="btn btn-warning">Edit</a>
-                                            <a href="#" data-id="{{ $price->id }}" id="btnHapusModal" class="btn btn-danger">Hapus</a>
+                                            <a href="#" data-id="{{ $price->id }}" id="btnEditModal"
+                                                class="btn btn-warning">Edit</a>
+                                            <a href="#" data-id="{{ $price->id }}" id="btnHapusModal"
+                                                class="btn btn-danger">Hapus</a>
                                         </td>
                                     </tr>
                                 @empty
@@ -73,8 +75,7 @@
     </div>
 
     <!-- Create Modal Price -->
-    <div class="modal fade" id="modal-create" tabindex="-1" role="dialog"
-        aria-hidden="true">
+    <div class="modal fade" id="modal-create" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -100,17 +101,18 @@
                         </div>
                         <div class="mb-3">
                             <label for="custom_text_button" class="form-label">Custom Text Button (lihat hasil)</label>
-                            <input type="text" class="form-control" name="custom_text_button" id="custom_text_button" placeholder="">
+                            <input type="text" class="form-control" name="custom_text_button" id="custom_text_button"
+                                placeholder="">
                         </div>
                         <div class="mb-3">
-                            {{-- <label for="highlight_harga" class="form-label">Highlight Harga</label> --}}
-                            {{-- <input type="checkbox" class="form-control" name="highlight_harga" id="highlight_harga" placeholder=""> --}}
                             <div class="custom-control custom-switch">
-                                <input type="checkbox" class="custom-control-input" value="on" name="highlight_harga" id="highlight_harga">
+                                <input type="checkbox" class="custom-control-input" value="on" name="highlight_harga"
+                                    id="highlight_harga">
                                 <label class="custom-control-label" for="highlight_harga">Highlight Harga ?</label>
-                              </div>
+                            </div>
                         </div>
-                        <button class="btn btn-md btn-primary" id="btnSimpanHarga"><i class="fas fa-fw fa-save"></i>Simpan</button>
+                        <button class="btn btn-md btn-primary" id="btnSimpanHarga"><i
+                                class="fas fa-fw fa-save"></i>Simpan</button>
                     </form>
                 </div>
             </div>
@@ -118,8 +120,7 @@
     </div>
 
     <!-- Edit Modal Category -->
-    <div class="modal fade" id="modal-edit" tabindex="-1" role="dialog"
-        aria-hidden="true">
+    <div class="modal fade" id="modal-edit" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -144,22 +145,25 @@
                             <trix-editor id="trx_ket_edit" input="keterangan_edit"></trix-editor>
                         </div>
                         <div class="mb-3">
-                            <label for="custom_text_button_edit" class="form-label">Custom Text Button (lihat hasil)</label>
-                            <input type="text" class="form-control" name="custom_text_button_edit" id="custom_text_button_edit">
+                            <label for="custom_text_button_edit" class="form-label">Custom Text Button (lihat
+                                hasil)</label>
+                            <input type="text" class="form-control" name="custom_text_button_edit"
+                                id="custom_text_button_edit">
                         </div>
                         <div class="mb-3">
                             <div class="custom-control custom-switch">
-                                <input type="checkbox" class="custom-control-input" value="on" name="highlight_harga_edit" id="highlight_harga_edit">
+                                <input type="checkbox" class="custom-control-input" value="on"
+                                    name="highlight_harga_edit" id="highlight_harga_edit">
                                 <label class="custom-control-label" for="highlight_harga_edit">Highlight Harga ?</label>
                             </div>
                         </div>
-                        <button class="btn btn-md btn-primary" id="btnUpdateHarga"><i class="fas fa-fw fa-save"></i>Update</button>
+                        <button class="btn btn-md btn-primary" id="btnUpdateHarga"><i
+                                class="fas fa-fw fa-save"></i>Update</button>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-
 @endsection
 @push('scripts')
     <script src="{{ url('sb-admin') }}/vendor/datatables/jquery.dataTables.min.js"></script>
@@ -170,15 +174,16 @@
             e.preventDefault();
         });
 
-        $(document).ready(function(){
+        $(document).ready(function() {
             let ids;
 
-            $('body').on('click', '#btnSimpanHarga', function (e) {
+            /* FUNGSI TAMBAH HARGA */
+            $('body').on('click', '#btnSimpanHarga', function(e) {
                 e.preventDefault()
                 $.ajax({
                     url: '{{ route('prices.store') }}',
                     method: 'POST',
-                    data:{
+                    data: {
                         _token: '{{ csrf_token() }}',
                         judul: $('#judul').val(),
                         harga: $('#harga').val(),
@@ -186,20 +191,21 @@
                         custom_text_button: $('#custom_text_button').val(),
                         is_featured: $('#highlight_harga').prop('checked') ?? false,
                     },
-                    success: function (data) {
+                    success: function(data) {
                         $('#judul').val('')
                         $('#harga').val('')
                         $('#keterangan').val('')
                         $('#custom_text_button').val('')
                         $('#modal-create').modal('hide')
                     },
-                    error: function (err) {
+                    error: function(err) {
                         console.log(err);
                     }
                 })
             })
 
-            $('body').on('click','#btnEditModal', function (e) {
+            /* FUNGSI GET HARGA BY ID */
+            $('body').on('click', '#btnEditModal', function(e) {
                 e.preventDefault()
                 $('#modal-edit').modal('show')
 
@@ -218,13 +224,14 @@
 
             })
 
-            $('body').on('click', '#btnUpdateHarga', function (e) {
+            /* FUNGSI UPDATE HARGA */
+            $('body').on('click', '#btnUpdateHarga', function(e) {
                 e.preventDefault()
-                
+
                 $.ajax({
                     url: `${window.location.pathname}/${ids}`,
                     method: 'PUT',
-                    data:{
+                    data: {
                         _token: '{{ csrf_token() }}',
                         judul: $('#judul_edit').val(),
                         harga: $('#harga_edit').val(),
@@ -232,7 +239,7 @@
                         custom_text_button: $('#custom_text_button_edit').val(),
                         is_featured: $('#highlight_harga_edit').prop('checked') ?? false,
                     },
-                    success: function (res) {
+                    success: function(res) {
                         console.log(res);
                         $('#judul_edit').val('')
                         $('#harga_edit').val('')
@@ -240,25 +247,28 @@
                         $('#custom_text_button_edit').val('')
                         $('#modal-edit').modal('hide')
                     },
-                    error: function (err) {
+                    error: function(err) {
                         console.log(err);
                     }
                 })
             })
 
-            $('body').on('click', '#btnHapusModal', function (e) {
+            /* FUNGSI HAPUS DATA */
+            $('body').on('click', '#btnHapusModal', function(e) {
                 e.preventDefault()
                 ids = $(this).data('id')
 
                 $.ajax({
                     url: `${window.location.pathname}/${ids}`,
                     method: 'DELETE',
-                    data:{
+                    data: {
                         _token: '{{ csrf_token() }}',
                         id: ids
-                    },success:function(res){
+                    },
+                    success: function(res) {
                         console.log(res);
-                    },error:function(err){
+                    },
+                    error: function(err) {
                         console.log(err);
                     }
                 })
