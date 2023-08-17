f192ad6089f96abcba48563e281cb801209609fe
kmmaniez <dzlabsid@gmail.com>
2023-08-18 03:08:20 +0700

    add carousel controller

---
+++ app/Http/Controllers/Admin/Website/CarouselController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Website;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Website\CarouselRequest;
+use App\Http\Requests\Website\StoreCarouselRequest;
+use App\Models\Carousel;
+use Illuminate\Http\Request;
+
+class CarouselController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CarouselRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Carousel $carousel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Carousel $carousel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Carousel $carousel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Carousel $carousel)
+    {
+        //
+    }
+}
