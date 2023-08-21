52f28f68d6023fcefad01eab215f2667bcb1f152
kmmaniez <dzlabsid@gmail.com>
2023-08-21 16:24:49 +0700

    update dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 Use Alert;
+use App\Models\Blog\KategoriPost;
+use App\Models\Blog\Post;
+use App\Models\Clients;
+use App\Models\Project\Project;
+use App\Models\User;
+
 class DashboardController extends Controller
 {
     public function index()
     {
-        // toast('Signed in successfully','success')->timerProgressBar();
-        return view('admin.dashboard');
+        return view('admin.dashboard',[
+            'totalpost' => Post::all()->count(),
+            'totalkatpost' => KategoriPost::all()->count(),
+            'totaluser' => User::all()->count(),
+            'totalclient' => Clients::all()->count(),
+            'totalproject' => Project::all()->count(),
+        ]);
     }
 }
