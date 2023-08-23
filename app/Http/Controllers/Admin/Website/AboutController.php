0261f634f24862b67595cf3525d9ed7a0a92214a
kmmaniez <dzlabsid@gmail.com>
2023-08-23 20:38:16 +0700

    update about us controller

--- app/Http/Controllers/Admin/Website/AboutController.php
+++ app/Http/Controllers/Admin/Website/AboutController.php
@@ -3,9 +3,33 @@
 namespace App\Http\Controllers\Admin\Website;
 
 use App\Http\Controllers\Controller;
+use App\Models\WebsiteSetting;
 use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
-    //
+    /* FUNGSI VIEWS HALAMAN ABOUT US */
+    public function index()
+    {
+        return view('admin.web.about', [
+            'title'     => 'About Us Settings',
+            'jmlhdata'  => WebsiteSetting::count(),
+            'data'      => WebsiteSetting::all(),
+        ]);
+    }
+
+    /* FUNGSI SIMPAN ABOUT US */
+    public function store(Request $request)
+    {
+        WebsiteSetting::create($request->all());
+        return redirect()->back();
+    }
+
+    /* FUNGSI UPDATE ABOUT US */
+    public function update(Request $request, $id)
+    {
+        $data = WebsiteSetting::firstWhere('id',$id);
+        $data->update($request->except('aboutid'));
+        return redirect()->back();
+    }
 }
