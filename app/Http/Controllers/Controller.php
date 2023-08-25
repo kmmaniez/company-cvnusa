561993f261e5cb01400985446d0f92b7231904cd
kmmaniez <dzlabsid@gmail.com>
2023-08-25 21:46:55 +0700

    add function to handle response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -10,4 +11,39 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
+    /* 
+    * array $data, data yang akan dikirim
+    * string $type, menandakan aksi yang terjadi
+    * int $code, kode response dari server
+    */
+    public function sendResponse(array $data, string $type = 'created', int $code = 200)
+    {
+        $response = [
+            ...$data,
+            'message'   => 'Data '.$type.' successfully!',
+            'code'      => $code,
+        ];
+
+        switch ($type) {
+            case 'created':
+                return response()->json([
+                    ...$response,
+                ]);
+                break;
+                
+            case 'updated':
+                return response()->json([
+                    ...$response,
+                ]);
+                break;
+
+            case 'deleted':
+                return response()->json([
+                    ...$response,
+                ]);
+                break;
+            default:
+                break;
+        }
+    }
 }
