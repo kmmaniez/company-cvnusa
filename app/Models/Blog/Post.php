73e844f9b6f54948b81fadf16e9ca975b0aa0acd
kmmaniez <dzlabsid@gmail.com>
2023-08-28 12:45:21 +0700

    update model post

--- app/Models/Blog/Post.php
+++ app/Models/Blog/Post.php
@@ -3,12 +3,13 @@
 namespace App\Models\Blog;
 
 use App\Models\User;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
     protected $guarded = ['id'];
     protected $with = ['kategoris'];
 
@@ -20,5 +21,19 @@ class Post extends Model
     public function users()
     {
         return $this->belongsTo(User::class,'user_id');
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+    
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
