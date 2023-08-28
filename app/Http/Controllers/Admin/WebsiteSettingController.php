03f809d8f1cbe57f7834c75611032a02b83e7e01
kmmaniez <dzlabsid@gmail.com>
2023-08-28 12:51:49 +0700

    update website setting controller

--- app/Http/Controllers/Admin/WebsiteSettingController.php
+++ app/Http/Controllers/Admin/WebsiteSettingController.php
@@ -31,42 +31,6 @@ class WebsiteSettingController extends Controller
         ]);
     }
 
-    /* FUNGI VIEWS HALAMAN WALLPAPER */
-    public function indexWallpaper()
-    {
-        return view('admin.web.wallpaper', [
-            'title' => 'Wallpaper Image',
-            'data' => $this->_wallpaper->all()
-        ]);
-    }
-
-    /* FUNGSI GET ALL WALLPAPER DATATABLES */
-    public function getAllWallpaper(Request $request)
-    {
-        if ($request->ajax()) {
-            $model = Wallpaper::all();
-    
-            return DataTables::of($model)
-                        ->editColumn('section_name', function($row){
-                            $str = '<strong>'.strtoupper($row->section_name).'</strong>';
-                            return $str;
-                        })
-                        ->editColumn('wallpaper_image', function($row){
-                            return view('admin.web.wallpaperdt', [
-                                'images' => $row->wallpaper_image
-                            ]);
-                        })
-                        ->editColumn('action', function($row){
-                            $btn = '
-                            <a href="#" id="btnEditWallpaper" data-wall="'.$row->id.'" class="btn btn-md btn-info"><i class="fas fa-fw fa-edit"></i> Edit</a>
-                            ';
-                            return $btn;
-                        })
-                        ->rawColumns(['section_name','action'])
-                        ->toJson();
-        }
-    }
-
     /**
      * Show the form for creating a new resource.
      */
@@ -84,7 +48,6 @@ class WebsiteSettingController extends Controller
         return redirect()->back();
     }
 
-
     /**
      * Display the specified resource.
      */
@@ -93,51 +56,12 @@ class WebsiteSettingController extends Controller
         //
     }
 
-    public function showWallpaperById($wallpaper)
-    {
-        $web = $this->_wallpaper->find($wallpaper);
-        return response()->json([
-            'data' => $web
-        ]);
-    }
-
-
     /* FUNGSI UPDATE ABOUT US */
     public function update(Request $request, $id)
     {
         $data = $this->_website->firstWhere('id',$id);
         $data->update($request->except('aboutid'));
         return redirect()->back();
-    }
-
-    /* FUNGSI UPDATE WALLPAPER */
-    public function updateWallpaper(Request $request, $id)
-    {
-        
-        if ($request->ajax()) {
-            $data = $this->_wallpaper->firstWhere('id', $id);
-            if ($request->has('wallpaper_image')) {
-
-                if ($data->wallpaper_image != NULL) {
-                    Storage::delete($data->wallpaper_image); // HAPUS FOTO LAMA
-                }
-
-                $imgName = date('His_dmY').'-'.strtolower($data->section_name).'.'.$request->wallpaper_image->extension();
-                $pathName = Storage::putFileAs('public/wallpaper', $request->file('wallpaper_image'), $imgName);
-                
-                $data = $data->update([
-                    'wallpaper_image' => $pathName
-                ]);
-
-                if ($data) {
-                    return response()->json([
-                        'data' => $data,
-                        'messages' => 'Data successfully changed',
-                        'req' => $request
-                    ]);
-                }
-            }
-        }
     }
     
     public function destroy(WebsiteSetting $websiteSetting)
