c98916167cc178c2ede576a94293d78ce05409e8
kmmaniez <dzlabsid@gmail.com>
2023-08-28 12:49:35 +0700

    update image datatable post

--- resources/views/admin/blog/imagedt.blade.php
+++ resources/views/admin/blog/imagedt.blade.php
@@ -1,5 +1,5 @@
 @props(['gambar'])
 <img src="{{ 
-    ($gambar != NULL) ? url("assets/images/services/$gambar") : url("assets/images/services/service1.jpg") }}" 
+    ($gambar != NULL) ? Storage::url($gambar) : url("assets/images/services/service1.jpg") }}" 
     style="object-fit: cover;" width="auto" height="100" alt="gambar-service"
 >
