9e8661cffe0d85b1fe6d0d753c908b77c8673e47
kmmaniez <dzlabsid@gmail.com>
2023-08-30 02:07:06 +0700

    view single posts

--- resources/views/public/blog/post.blade.php
+++ resources/views/public/blog/post.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 
 <section id="main-container" class="main-container">
-    {{-- @dump($post) --}}
+
     <div class="container">
         <div class="row">
 
+            <!-- POST -->
             <div class="col-lg-8 mb-5 mb-lg-0">
-
                 <div class="post-content post-single">
                     <div class="post-media post-image">
                         <img loading="lazy" src="{{ (isset($post->thumbnail) ? Storage::url($post->thumbnail) : asset('assets/images/news/news1.jpg')) }}" class="img-fluid" alt="post-image">
@@ -25,52 +25,19 @@
                             <h2 class="entry-title">
                                 {{ $post->title }}
                             </h2>
-                        </div><!-- header end -->
+                        </div>
 
                         <div class="entry-content">
                             {!! $post->content !!}
                         </div>
 
-                    </div><!-- post-body end -->
-                </div><!-- post content end -->
+                    </div>
+                </div>
+            </div>
 
-
-            </div><!-- Content Col end -->
-
+            <!-- RECENT POST & CATEGORIES -->
             <div class="col-lg-4">
-
-                <div class="sidebar sidebar-right">
-                    <div class="widget recent-posts">
-                        <h3 class="widget-title">Recent Posts</h3>
-                        <ul class="list-unstyled">
-                            @foreach ($recentposts as $post)
-                                <li class="d-flex align-items-center">
-                                    <div class="posts-thumb">
-                                        <a href="{{ route('public.post.detail', $post->slug) }}"><img loading="lazy" alt="img"
-                                                src="{{ (isset($post->thumbnail) ? Storage::url($post->thumbnail) : asset('assets/images/news/news1.jpg')) }}"></a>
-                                    </div>
-                                    <div class="post-info">
-                                        <h4 class="entry-title">
-                                            <a href="{{ route('public.post.detail', $post->slug) }}">{{ $post->title }}</a>
-                                        </h4>
-                                    </div>
-                                </li>
-                            @endforeach
-                            
-                        </ul>
-
-                    </div><!-- Recent post end -->
-
-                    <div class="widget">
-                        <h3 class="widget-title">Categories</h3>
-                        <ul class="arrow nav nav-tabs">
-                            @foreach ($kategori as $kat)
-                            <li><a href="#">{{ $kat->nama_kategori }}</a></li>
-                            @endforeach
-                        </ul>
-                    </div><!-- Categories end -->
-
-                </div><!-- Sidebar end -->
+                <x-public.sidebarblog :recentposts="$recentposts" :kategori="$kategori"></x-public.sidebarblog>
             </div><!-- Sidebar Col end -->
 
         </div><!-- Main row end -->
