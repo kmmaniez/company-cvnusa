bf5ddcba25f164c61082901c06e770c993a94d74
kmmaniez <dzlabsid@gmail.com>
2023-08-30 02:48:53 +0700

    update public routes

--- routes/web/public.php
+++ routes/web/public.php
@@ -13,6 +13,13 @@ Route::controller(PublicController::class)->as('public.')->group(function () {
     Route::get('/clients', 'clients')->name('clientpublic');
     Route::get('/contact-us', 'contact')->name('contact');
 
+    // PROJECT
     Route::get('/projects', 'projects')->name('projects');
     Route::get('/projects/id', 'project_details')->name('project-single');
+
+    // BLOG
+    Route::get('/blog', 'posts')->name('post.all');
+    Route::get('/blog/kategori/{kategoriPost:nama_kategori}','kategoripost');
+    Route::get('/blog/{post:slug}', 'posts')->name('post.detail');
+
 });
