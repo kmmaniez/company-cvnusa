86635fb8e9353677d9687222b860670908b19867
kmmaniez <dzlabsid@gmail.com>
2023-08-31 22:09:23 +0700

    final component carousel

--- resources/views/components/public/carousel.blade.php
+++ resources/views/components/public/carousel.blade.php
@@ -2,7 +2,7 @@
 
 <div class="banner-carousel banner-carousel-1 mb-0">
 
-    @foreach ($datacarousel as $carousel)
+    @forelse ($datacarousel as $carousel)
     <div class="banner-carousel-item" style="background-image:url({{ ($carousel->image) ? Storage::url($carousel->image) : url('assets/images/slider-main/bg1.jpg') }})">
         <div class="slider-content">
             <div class="container h-100">
@@ -16,6 +16,20 @@
             </div>
         </div>
     </div>
-    @endforeach
+    @empty
+    <div class="banner-carousel-item" style="background-image:url('assets/images/slider-main/bg1.jpg')">
+        <div class="slider-content">
+            <div class="container h-100">
+                <div class="row align-items-center h-100">
+                    <div class="col-md-12 text-center">
+                        <h2 class="slide-title" data-animation-in="slideInLeft">Title</h2>
+                        <h3 class="slide-sub-title" data-animation-in="slideInRight">SubTitle</h3>
+                        <p class="slider-description lead" data-animation-in="slideInRight">Description</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endforelse
 
 </div>
