49a1ae6caf3446b69079647c4f924f3621c2c22f
kmmaniez <dzlabsid@gmail.com>
2023-08-31 22:07:43 +0700

    update public contact

--- resources/views/public/contact.blade.php
+++ resources/views/public/contact.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.public.master')
 
 @section('content')
-    <div id="banner-area" class="banner-area" style="background-image:url({{ url('assets/images/banner/banner1.jpg') }})">
+    <div id="banner-area" class="banner-area" style="background-image:url(
+        @if (count($wallpaper) > 0 && $wallpaper[0]->wallpaper_image != null)
+            {{ Storage::url($wallpaper[0]->wallpaper_image) }}
+        @else
+            {{ url('assets/images/banner/banner1.jpg')  }}
+        @endif
+    )">
         <div class="banner-text">
             <div class="container">
                 <div class="row">
