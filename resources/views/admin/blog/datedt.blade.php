5f3ac69b8a1000e7abb5da1f248bc1d3dfc1ff8d
kmmaniez <dzlabsid@gmail.com>
2023-09-01 18:12:36 +0700

    update date datatable view

--- resources/views/admin/blog/datedt.blade.php
+++ resources/views/admin/blog/datedt.blade.php
@@ -1,5 +1,8 @@
 @props(['created','updated'])
 
-<span class="badge badge-primary p-2">Dibuat {{ $created }}</span>
+<span class="badge badge-primary p-2">Dibuat {{ \Carbon\Carbon::now('Asia/Jakarta')->parse($created)->translatedFormat('l, d F Y') }}</span>
 <br>
-<span class="badge badge-secondary p-2 mt-1">Diupdate {{ $updated }}</span>
+
+@if (isset($updated))
+<span class="badge badge-secondary p-2 mt-1">Diupdate {{ \Carbon\Carbon::now('Asia/Jakarta')->parse($updated)->translatedFormat('l, d F Y') }}</span>
+@endif
