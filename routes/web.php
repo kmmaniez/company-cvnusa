9f29b3c1f8386426914c4c20d1e28bf78c24eb13
kmmaniez <dzlabsid@gmail.com>
2023-09-02 21:43:11 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -39,80 +39,83 @@ use function PHPUnit\Framework\directoryExists;
 
 
 /* PRIVATE ROUTES (WITH AUTH) */
-// Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth'])->group(function () {
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-// ALL ROUTE WITH PREFIX SETTINGS, EX: localhost/settings/clients etc
-Route::prefix('settings')->group(function () {
+    // ALL ROUTE WITH PREFIX SETTINGS, EX: localhost/settings/clients etc
+    Route::prefix('settings')->group(function () {
 
-    // ROUTE RESOURCE CLIENT,KATEGORI,PROJECT,TEAM,PRICE
-    /* accessed with ex: localhost/settings/clients/index */
-    Route::resources([
-        'kategoriproject'   => KategoriProjectController::class,
-        'projects'          => ProjectController::class,
-        'teams'             => AnggotaController::class,
-        'kategorijabatan'   => KategoriJabatanController::class,
-        'prices'            => PriceController::class,
-        'services'          => ServiceController::class,
-    ]);
-
-    Route::resource('clients', ClientsController::class)->only([
-        'index','store','show','update','destroy'
-    ]);
-
-
-    // ROUTE GROUP WEBSITE SETTING
-    /* accessed with prefix website, ex: localhost/settings/landing-page/about-us */
-    Route::controller(WebsiteSettingController::class)->prefix('landing-page')->group(function () {
-
-        // MENU ABOUT-US
-        Route::controller(AboutController::class)->prefix('about-us')->group(function(){
-            Route::get('/', 'index')->name('about.index');
-            Route::post('/', 'store')->name('about.store');
-            Route::put('/{id}', 'update')->name('about.update');
+        // ROUTE RESOURCE CLIENT,KATEGORI,PROJECT,TEAM,PRICE
+        /* accessed with ex: localhost/settings/clients/index */
+        Route::middleware(['can:manage-projects'])->group(function () {
+            Route::resource('kategoriproject', KategoriProjectController::class);
+            Route::resource('projects', ProjectController::class);
         });
-        
-        // MENU CAROUSEL
-        Route::resource('carousels', CarouselController::class)->only([
-            'index','store','show','update','destroy'
-        ]);
 
-        // MENU WALLPAPER
-        Route::controller(WallpaperController::class)->prefix('wallpapers')->group(function(){
-            Route::get('/','index')->name('wallpaper.index');
-            Route::get('/getallwallpaper','getAllWallpaper')->name('wallpaper.getallwallpaper');
-            Route::get('/{wallpaper}','show')->name('wallpaper.show');
-            Route::patch('/{wallpaper}','update')->name('wallpaper.update');
+        Route::middleware(['can:manage-teams'])->group(function () {
+            Route::resource('teams', AnggotaController::class);
+            Route::resource('kategorijabatan', KategoriJabatanController::class);
         });
+
+        Route::resource('prices', PriceController::class)->middleware('can:manage-prices');
+
+        Route::resource('services', ServiceController::class)->middleware('can:manage-services');
+
+        Route::resource('clients', ClientsController::class)->middleware('can:manage-clients');
+
+
+        // ROUTE GROUP WEBSITE SETTING
+        /* accessed with prefix website, ex: localhost/settings/landing-page/about-us */
+        Route::controller(WebsiteSettingController::class)->middleware('can:manage-websites')->prefix('landing-page')->group(function () {
+
+            // MENU ABOUT-US
+            Route::controller(AboutController::class)->prefix('about-us')->group(function(){
+                Route::get('/', 'index')->name('about.index');
+                Route::post('/', 'store')->name('about.store');
+                Route::put('/{id}', 'update')->name('about.update');
+            });
+            
+            // MENU CAROUSEL
+            Route::resource('carousels', CarouselController::class)->only([
+                'index','store','show','update','destroy'
+            ]);
+
+            // MENU WALLPAPER
+            Route::controller(WallpaperController::class)->prefix('wallpapers')->group(function(){
+                Route::get('/','index')->name('wallpaper.index');
+                Route::get('/getallwallpaper','getAllWallpaper')->name('wallpaper.getallwallpaper');
+                Route::get('/{wallpaper}','show')->name('wallpaper.show');
+                Route::patch('/{wallpaper}','update')->name('wallpaper.update');
+            });
+
+        });
+
+
+        // ROUTE GROUP USER
+        /* accessed with prefix users, ex: localhost/settings/users/index */
+        Route::controller(UserController::class)->prefix('users')->as('users.')->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::post('/', 'store')->name('store');
+            Route::patch('/{user}', 'update')->name('update');
+            Route::delete('/destroy/{user}', 'destroy')->name('destroy');
+
+            Route::get('/getdatausers', 'getAllUsers')->name('getusers');
+            Route::get('/getuser/{id?}', 'getUserById')->name('getuserbyid');
+
+            Route::get('/checkmail/{id?}', 'checkEmailAndUsername')->name('checkmailusername');
+            Route::get('/check', 'awewe');
+        });
+
+
+        // ROUTE GROUP BLOG
+        /* acessed with prefix blogs, ex: localhost/settings/blogs/posts/index */
+        require __DIR__ . '/web/blog.php'; // ROUTE BLOG
 
     });
 
-
-    // ROUTE GROUP USER
-    /* accessed with prefix users, ex: localhost/settings/users/index */
-    Route::controller(UserController::class)->prefix('users')->as('users.')->group(function () {
-        Route::get('/', 'index')->name('index');
-        Route::post('/', 'store')->name('store');
-        Route::patch('/{user}', 'update')->name('update');
-        Route::delete('/destroy/{user}', 'destroy')->name('destroy');
-
-        Route::get('/getdatausers', 'getAllUsers')->name('getusers');
-        Route::get('/getuser/{id?}', 'getUserById')->name('getuserbyid');
-
-        Route::get('/checkmail/{id?}', 'checkEmailAndUsername')->name('checkmailusername');
-        Route::get('/check', 'awewe');
-    });
-
-
-    // ROUTE GROUP BLOG
-    /* acessed with prefix blogs, ex: localhost/settings/blogs/posts/index */
-    require __DIR__ . '/web/blog.php'; // ROUTE BLOG
 
 });
-
-
-// });
 
 
 Route::get('/datakategori', function () {
