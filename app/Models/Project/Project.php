10aa8a81171fd1f098a38f5331eae8d8274f39f2
kmmaniez <dzlabsid@gmail.com>
2023-09-17 22:02:34 +0700

    add sluggable to model

--- app/Models/Project/Project.php
+++ app/Models/Project/Project.php
@@ -2,11 +2,26 @@
 
 namespace App\Models\Project;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
     protected $guarded = ['id'];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+    
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'nama_project'
+            ]
+        ];
+    }
 }
