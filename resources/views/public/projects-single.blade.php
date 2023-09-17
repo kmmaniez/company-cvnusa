60fc671ffd6955d155a10d740cf3272f9e667a22
kmmaniez <dzlabsid@gmail.com>
2023-09-17 22:05:56 +0700

    update views project single

--- resources/views/public/projects-single.blade.php
+++ resources/views/public/projects-single.blade.php
@@ -7,14 +7,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="banner-heading">
-                            <h1 class="banner-title">Project</h1>
-                            <nav aria-label="breadcrumb">
-                                <ol class="breadcrumb justify-content-center">
-                                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                    <li class="breadcrumb-item"><a href="#">Project</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">Projects Single</li>
-                                </ol>
-                            </nav>
+                            <h1 class="banner-title">Project {{ $data->nama_project }}</h1>
                         </div>
                     </div><!-- Col end -->
                 </div><!-- Row end -->
@@ -27,23 +20,23 @@
 
             <div class="row">
                 <div class="col-lg-8">
-                    <div id="page-slider" class="page-slider small-bg">
-                        <div class="item">
-                            <img loading="lazy" class="img-fluid" src="{{ url('assets/images/projects/project5.jpg') }}"
-                                alt="project-image" />
-                        </div>
 
+                    <div id="page-slider" class="page-slider small-bg">
+
+                        @foreach (json_decode($data->gambar_project) as $gambar)
                         <div class="item">
-                            <img loading="lazy" class="img-fluid" src="{{ url('assets/images/projects/project4.jpg') }}"
+                            <img loading="lazy" class="img-fluid" src="{{ Storage::url($gambar) }}"
                                 alt="project-image" />
                         </div>
-                    </div><!-- Page slider end -->
-                </div><!-- Slider col end -->
+                        @endforeach
+                    </div>
+
+                </div>
 
                 <div class="col-lg-4 mt-5 mt-lg-0">
 
-                    <h3 class="column-title mrt-0">Capital Teltway Building</h3>
-                    <p>Morbi turpis nisl, auctor ut nisl vel, pellentesque euismod nunc nunc accumsan imperdiet.</p>
+                    <h3 class="column-title mrt-0">{{ $data->nama_project }}</h3>
+                    <p>{{ $data->keterangan_project }}</p>
 
                     <ul class="project-info list-unstyled">
                         <li>
@@ -51,20 +44,8 @@
                             <p class="project-info-content">Pransbay Powers Authority</p>
                         </li>
                         <li>
-                            <p class="project-info-label">Architect</p>
-                            <p class="project-info-content">Dlarke Pelli Incorp</p>
-                        </li>
-                        <li>
-                            <p class="project-info-label">Location</p>
-                            <p class="project-info-content">McLean, VA</p>
-                        </li>
-                        <li>
-                            <p class="project-info-label">Size</p>
-                            <p class="project-info-content">65,000 SF</p>
-                        </li>
-                        <li>
                             <p class="project-info-label">Year Completed</p>
-                            <p class="project-info-content">2015</p>
+                            <p class="project-info-content">{{ $data->finish_project }}</p>
                         </li>
                         <li>
                             <p class="project-info-label">Categories</p>
