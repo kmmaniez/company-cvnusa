9613938ff41b7b0085b92a84400d2546b691f366
kmmaniez <dzlabsid@gmail.com>
2023-09-19 20:13:40 +0700

    add informasi request

---
+++ app/Http/Requests/Website/InformasiRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Website;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class InformasiRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+            'telepon' => 'required|numeric',
+            'alamat' => 'required|string',
+            'logo' => 'nullable|image|mimes:png,jpg,jpeg|max:2048'
+        ];
+    }
+}
