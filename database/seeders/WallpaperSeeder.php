7fd3b9fbb6d7fc53068389b0dc1fc4d384133649
kmmaniez <dzlabsid@gmail.com>
2023-09-21 20:40:14 +0700

    update wallpaper seeder

--- database/seeders/WallpaperSeeder.php
+++ database/seeders/WallpaperSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Wallpaper;
+use App\Models\Website\Wallpaper;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
