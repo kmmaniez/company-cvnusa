c4593985eb9bfcd4a239a2fbf2178187ebaa16bb
kmmaniez <dzlabsid@gmail.com>
2023-09-22 18:16:20 +0700

    add fillable to model service

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -8,5 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Service extends Model
 {
     use HasFactory;
-    protected $guarded = ['id'];
+
+    protected $fillable = [
+        'title',
+        'description',
+        'gambar',
+    ];
 }
