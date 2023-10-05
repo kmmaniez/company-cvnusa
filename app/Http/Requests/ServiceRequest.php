57e065c49169951bab1f82b7f96346b31a47581c
kmmaniez <dzlabsid@gmail.com>
2023-10-05 11:20:29 +0700

    add service request

---
+++ app/Http/Requests/ServiceRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ServiceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nama_service' => ['required'],
+            'gambar_service' => ['nullable','image','mimes:png,jpg,jpeg'],
+            'description_service' => ['required'],
+        ];
+    }
+}
