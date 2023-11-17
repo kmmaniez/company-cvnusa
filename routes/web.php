0d8c59ae93576851a3ea960d17db4703876eef7c
kmmaniez <dzlabsid@gmail.com>
2023-11-17 21:23:06 +0700

    final routes

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,24 @@
 
 use App\Http\Controllers\Admin\Blog\KategoriPostController;
 use App\Http\Controllers\Admin\Blog\PostController;
+use App\Http\Controllers\Admin\ClientsController;
 use App\Http\Controllers\Admin\Team\AnggotaController;
-use App\Http\Controllers\ClientsController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\PriceController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\PriceController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\Projects\ProjectController;
 use App\Http\Controllers\Admin\Team\KategoriJabatanController;
-use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\Admin\ServiceController;
 use App\Http\Controllers\Admin\User\UserController;
 use App\Http\Controllers\Admin\WebsiteSettingController;
 use App\Http\Controllers\Admin\Projects\KategoriProjectController;
 use App\Http\Controllers\Admin\Website\AboutController;
 use App\Http\Controllers\Admin\Website\CarouselController;
+use App\Http\Controllers\Admin\Website\InformasiController;
 use App\Http\Controllers\Admin\Website\WallpaperController;
 use App\Models\Clients;
 use App\Models\Kategori;
-use App\Models\User;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
 use Yajra\DataTables\Facades\DataTables;
 
 use function PHPUnit\Framework\directoryExists;
@@ -46,51 +44,94 @@ Route::middleware(['auth'])->group(function () {
     // ALL ROUTE WITH PREFIX SETTINGS, EX: localhost/settings/clients etc
     Route::prefix('settings')->group(function () {
 
-        // ROUTE RESOURCE CLIENT,KATEGORI,PROJECT,TEAM,PRICE
+        // ROUTE RESOURCE KATEGORI,PROJECT
         /* accessed with ex: localhost/settings/clients/index */
         Route::middleware(['can:manage-projects'])->group(function () {
             Route::resource('kategoriproject', KategoriProjectController::class);
-            Route::resource('projects', ProjectController::class);
+
+            // ROUTE POST BLOG
+            Route::prefix('projects')->group(function () {
+
+                Route::controller(ProjectController::class)->group(function () {
+                    Route::get('/', 'index')->name('projects.index');
+                    Route::get('/create', 'create')->name('projects.create');
+                    Route::post('/', 'store')->name('projects.store');
+
+                    Route::get('/{project}/edit', 'edit')->name('projects.edit');
+                    Route::patch('/{project}', 'update')->name('projects.update');
+                    Route::delete('/{project}', 'destroy')->name('projects.destroy');
+                    Route::delete('/delete-image/{project}', 'deleteAllImages')->name('projects.deleteAllImages');
+
+                    Route::get('/checkslug', 'checkSlug')->name('projects.checkslug');
+                });
+            });
         });
 
+        // ROUTE GROUP TEAMS & KATEGORI JABATAN
         Route::middleware(['can:manage-teams'])->group(function () {
             Route::resource('teams', AnggotaController::class);
             Route::resource('kategorijabatan', KategoriJabatanController::class);
         });
 
-        Route::resource('prices', PriceController::class)->except('edit')->middleware('can:manage-prices');
+        // ROUTE GROUP PRICES
+        Route::middleware(['can:manage-prices'])->group(function () {
+            Route::resource('prices', PriceController::class)->except('edit');
+        });
 
-        Route::resource('services', ServiceController::class)->middleware('can:manage-services');
+        // ROUTE GROUP SERVICES
+        Route::middleware(['can:manage-services'])->group(function () {
+            Route::resource('services', ServiceController::class)->except('create');
+        });
+        
+        // ROUTE GROUP CLIENTS
+        /* accessed with prefix clients, ex: localhost/settings/clients/index */
+        Route::middleware(['can:manage-clients'])->group(function () {
 
-        Route::resource('clients', ClientsController::class)->middleware('can:manage-clients');
+            Route::controller(ClientsController::class)->group(function () {
+                Route::get('/clients', 'index')->name('clients.index');
+                Route::post('/clients', 'store')->name('clients.store');
+                Route::get('/clients/{client}', 'show')->name('clients.show');
 
+                Route::patch('/clients/{client}', 'update')->name('clients.update');
+                Route::delete('/clients/{client}', 'destroy')->name('clients.destroy');
+                // Route::get('/clients/getdataclients', 'getAllClients')->name('clients.getclients');
+            });
+            
+        });
 
         // ROUTE GROUP WEBSITE SETTING
         /* accessed with prefix website, ex: localhost/settings/landing-page/about-us */
-        Route::controller(WebsiteSettingController::class)->middleware('can:manage-websites')->prefix('landing-page')->group(function () {
-
-            // MENU ABOUT-US
-            Route::controller(AboutController::class)->prefix('about-us')->group(function(){
-                Route::get('/', 'index')->name('about.index');
-                Route::post('/', 'store')->name('about.store');
-                Route::put('/{id}', 'update')->name('about.update');
-            });
+        Route::middleware(['can:manage-websites'])->group(function () {
             
-            // MENU CAROUSEL
-            Route::resource('carousels', CarouselController::class)->only([
-                'index','store','show','update','destroy'
-            ]);
-
-            // MENU WALLPAPER
-            Route::controller(WallpaperController::class)->prefix('wallpapers')->group(function(){
-                Route::get('/','index')->name('wallpaper.index');
-                Route::get('/getallwallpaper','getAllWallpaper')->name('wallpaper.getallwallpaper');
-                Route::get('/{wallpaper}','show')->name('wallpaper.show');
-                Route::patch('/{wallpaper}','update')->name('wallpaper.update');
+            Route::controller(WebsiteSettingController::class)->prefix('landing-page')->group(function () {
+    
+                // MENU ABOUT-US
+                Route::controller(AboutController::class)->prefix('about-us')->group(function(){
+                    Route::get('/', 'index')->name('about.index');
+                    Route::post('/', 'store')->name('about.store');
+                    Route::put('/{id}', 'update')->name('about.update');
+                });
+                
+                // MENU CAROUSEL
+                Route::resource('carousels', CarouselController::class)->only([
+                    'index','store','show','update','destroy'
+                ]);
+    
+                // MENU WALLPAPER
+                Route::controller(WallpaperController::class)->prefix('wallpapers')->group(function(){
+                    Route::get('/','index')->name('wallpaper.index');
+                    Route::get('/getallwallpaper','getAllWallpaper')->name('wallpaper.getallwallpaper');
+                    Route::get('/{wallpaper}','show')->name('wallpaper.show');
+                    Route::patch('/{wallpaper}','update')->name('wallpaper.update');
+                });
+    
+                // MENU CAROUSEL
+                Route::resource('informasi', InformasiController::class)->only([
+                    'index','store','update'
+                ]);
+    
             });
-
         });
-
 
         // ROUTE GROUP USER
         /* accessed with prefix users, ex: localhost/settings/users/index */
@@ -118,50 +159,12 @@ Route::middleware(['auth'])->group(function () {
 });
 
 
-Route::get('/datakategori', function () {
-    return response()->json([
-        'data' => Kategori::all()
-    ]);
-});
-
-
-
-
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-/* ROUTE ONLY FOR DATATABLE */
-Route::prefix('datatables')->group(function () {
-
-    // DATA CLIENTS
-    Route::get('getdataclients', function () {
-        if (request()->ajax()) {
-            $model = Clients::all();
-            return DataTables::of($model)
-                ->editColumn('logo', function ($row) {
-                    // $img = '<img src="'.url('photos/client-logo', $row->logo).'" style="object-fit: cover;" width="300" height="150" alt="logo-client">';
-                    // // {{ url('photos/client-logo', $client->logo) }}
-                    // return $img;
-                    return view('admin.client.clientdt', [
-                        'logo' => $row->logo
-                    ]);
-                })
-                ->editColumn('action', function ($row) {
-                    $btn = '
-                        <a href="#" data-client="' . $row->id . '" id="btnEditClient" class="btn btn-md btn-info"><i class="fas fa-fw fa-edit"></i> Edit</a>
-                        <a href="#" data-client="' . $row->id . '" id="btnHapusClient" class="btn btn-md btn-danger"><i class="fas fa-fw fa-trash-alt"></i> Delete</a>
-                    ';
-                    return $btn;
-                })
-                ->toJson();
-        }
-        abort(404);
-    })->name('getdataclients');
-});
 
 require __DIR__ . '/web/public.php'; // ROUTE PUBLIC
 require __DIR__ . '/auth.php';
